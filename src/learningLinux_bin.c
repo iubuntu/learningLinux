543501468d5a391070197ff56b12ff5eb2a6095a
iubuntu <woyonglinux@gmail.com>
2024-05-14 10:43:18 +1000

    init the project

---
+++ src/learningLinux_bin.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("execute linux commands from binary files\n");
+    return 0;
+}
---
+++ src/learningLinux_text.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "execute linux commands from text files"
